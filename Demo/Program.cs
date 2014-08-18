b24901a50e8e392c7aa5357d08591b22cd756ac5
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-08-19 01:08:06 +0400

    Описание грамматики списка с левой рекурсией. Непротестировано.

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -20,18 +20,27 @@ namespace ParserCombinatorCs
             Console.ReadLine();
         }
 
-        static void List()
+        static ParserFn<object> Top()
         {
-            //ParserFn<IEnumerable<object>> list = null;
+            return List(0);
+        }
 
-            //var bullet = Parser.Char('-').Or(Parser.Char('+')).Or(Parser.Char('*'));
-            //var content = StateIndicators.isEol().Not().And(Parser.Char()).AtLeastOnce().And(StateIndicators.isEol());
-            //ParserFn<IEnumerable<object>> item = StateIndicators.Begin<char>()
-            //    .And(bullet)
-            //    .And(Parser.Char(' '))
-            //    .And(content)
-            //    .And(list.Optional());
-            //list = item.AtLeastOnce();
+        static ParserFn<object> List(int level)
+        {
+            return Item(level).AtLeastOnce().Select(objects => objects.Last());
+        }
+
+        static ParserFn<object> Item(int level)
+        {
+            ParserFn<char> bullet = Parser.Char('-').Or(Parser.Char('+')).Or(Parser.Char('*'));
+            ParserFn<object> content = StateIndicators.isEol().Not().And(Parser.Char()).AtLeastOnce().And(StateIndicators.isEol());
+            ParserFn<object> item = StateIndicators.Begin<char>()
+                .And(Parser.Char(' ').RepeatExactly(level).Join())
+                .And(bullet)
+                .And(Parser.Char(' '))
+                .And(content)
+                .And(List(level + 1).Optional());
+            return item;
         }
     }
 }
