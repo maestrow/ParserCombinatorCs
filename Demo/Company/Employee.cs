e061f1e2375bb271039a2aae4c2975f373aa2e0a
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-08-27 01:38:53 +0400

    -

---
+++ Demo/Company/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Company
+{
+    public class Employee
+    {
+        public PersonName Name { get; set; }
+
+        public PhoneNum Phone { get; set; }
+
+        public string Position { get; set; }
+    }
+}
---
+++ Demo/LeftRecursion/Grammar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Combinator;
+using Combinator.Helpers;
+
+namespace Demo.LeftRecursion
+{
+    public static class Grammar
+    {
+        public static string Test()
+        {
+            var state = new State("123");
+
+        }
+
+        public static ParserFn<int> Number()
+        {
+            return Parser.RegEx(@"\d").Select(a => int.Parse(a))
+        }
+    }
+}
